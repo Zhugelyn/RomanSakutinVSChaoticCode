0f12d7aa24963fe3f40b8ee15de77c6af3bbcdd9
putya9 <putinczev52@gmail.com>
2023-08-27 01:08:33 +0300

    Roman Attack
    Roman Attack

---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Transform _attackPoint;
+    [SerializeField] private float _attackRange = 0.5f;
+    [SerializeField] private LayerMask _enemyLayers;
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+            Attack();
+    }
+
+    private void Attack()
+    {
+        _animator.SetTrigger("Attack");
+
+        Collider2D[] hitEnemies =  Physics2D.OverlapCircleAll(_attackPoint.position, _attackRange, _enemyLayers);
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            //Здесь когда враги будут сделаем
+        }
+    }
+}
