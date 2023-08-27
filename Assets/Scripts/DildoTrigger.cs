994ad445046da4181e884bd1db93087e87dc0fa3
Zhugelyn <kibitkin_00@list.ru>
2023-08-27 22:20:37 +0400

    enemy

--- Assets/Scripts/DildoTrigger.cs
+++ Assets/Scripts/DildoTrigger.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class DildoTrigger : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer _renderer;
+
+    private int hp = 10;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer != 3)
@@ -13,7 +15,14 @@ public class DildoTrigger : MonoBehaviour
             Destroy(collision.gameObject);
             _renderer.color = Color.red;
             Invoke("ResetColor", 0.2f);
+            hp--;
         }
+
+        if (hp == 0)
+            Destroy(gameObject);
+
+        if (gameObject.layer == 6)
+            Destroy(gameObject);
     }
 
     private void ResetColor()
